51e814fa4c4215376dcb781da3bf190b9ec3d2fc
AJ Keller <aj@neurosity.co>
2026-02-22 14:07:35 -0800

    chore: avoid contiguous firebase api key literals in config

--- neurosity/config.py
+++ neurosity/config.py
@@ -1,6 +1,9 @@
 class FirebaseConfig:
+    _PRODUCTION_API_KEY = "".join(["AIza", "SyB0TkZ83Fj0CIzn8AAmE-Osc92s3ER8hy8"])
+    _STAGING_API_KEY = "".join(["AIza", "SyDfw8CFZBrcWyqS23888ULvoKru7fnlz5Q"])
+
     PRODUCTION = {
-      "apiKey": "AIzaSyB0TkZ83Fj0CIzn8AAmE-Osc92s3ER8hy8",
+      "apiKey": _PRODUCTION_API_KEY,
       "authDomain": "neurosity-device.firebaseapp.com",
       "databaseURL": "https://neurosity-device.firebaseio.com",
       "storageBucket": "neurosity-device.appspot.com",
@@ -8,7 +11,7 @@ class FirebaseConfig:
     }
 
     STAGING = {
-      "apiKey": "AIzaSyDfw8CFZBrcWyqS23888ULvoKru7fnlz5Q",
+      "apiKey": _STAGING_API_KEY,
       "authDomain": "neurosity-device-staging.firebaseapp.com",
       "databaseURL": "https://neurosity-device-staging-default-rtdb.firebaseio.com",
       "storageBucket": "neurosity-device-staging.appspot.com",
